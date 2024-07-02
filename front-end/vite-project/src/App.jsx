b23533766548457f76a028299c79e99ea99fb550
onumam21 <onumam56@gmail.com>
2024-07-02 12:14:00 +0700

    Change  SignUp

--- front-end/vite-project/src/App.jsx
+++ front-end/vite-project/src/App.jsx
@@ -1,12 +1,8 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
-import SignUp from ".pages/SignUp/SignUp.jsx";
+import SignUp from "./pages/SignUp/SignUp.jsx";
+
 function App() {
   return (
     <div>
@@ -21,16 +17,13 @@ function App() {
     </div>
   );
 }
+
 // Define the Root component to handle the initial redirect
 const Root = () => {
   // Check if token exists in localStorage
-  // The double negation operator is used to convert the result into a boolean.
   const isAuthenticated = !!localStorage.getItem("token");
   // Redirect to dashboard if authenticated, otherwise to login
-  return isAuthenticated ? (
-    <Navigate to="/dashboard" />
-  ) : (
-    <Navigate to="/login" />
-  );
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
 };
+
 export default App;
